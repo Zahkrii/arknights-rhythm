ba393fdc38be1249b467e9c19c793b49f42c2350
Zahkrii <32428600+Zahkrii@users.noreply.github.com>
2022-12-05 21:16:16 +0800

    添加存档管理器

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -1,18 +1,238 @@
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 using UnityEngine;
 
-public class SaveManager : MonoBehaviour
+public class SaveFile
 {
-    // Start is called before the first frame update
-    void Start()
+    public string playerID; //玩家ID
+    public List<ChartScore> chartScores; //分数列表
+    public List<Opreator> opreators; //已解锁/获得的干员列表
+}
+
+[Serializable]
+public class Opreator
+{
+    public string opreatorName; //干员代号
+}
+
+[Serializable]
+public class ChartScore
+{
+    public string name; //曲名
+
+    //简单难度（Easy）
+    public int paddingScoreEZ; //判定分，满分90万
+
+    public int comboScoreEZ; //连击分，满分10万
+
+    //普通难度（Normal）
+    public int paddingScoreNM; //判定分，满分90万
+
+    public int comboScoreNM; //连击分，满分10万
+
+    //困难难度（Hard）
+    public int paddingScoreHD; //判定分，满分90万
+
+    public int comboScoreHD; //连击分，满分10万
+
+    //超难难度（Insane）
+    public int paddingScoreIN; //判定分，满分90万
+
+    public int comboScoreIN; //连击分，满分10万
+
+    //FC：全连，连击分满分
+    //AP：All Perfect，判定分加连击分达到100万
+}
+
+public static class SaveManager
+{
+    //存档名
+    private const string FILENAME = "arknights_rhythm_CB05";
+
+    //存档扩展名
+    private const string FILENAME_EXTENSION = ".sav";
+
+    //存档特征签名
+    private const string FILE_MAGIC_NUMBER = "42";
+
+    public static SaveFile PlayerSave { get; private set; }
+
+    /// <summary>
+    /// 初始化存档文件
+    /// </summary>
+    /// <param name="playerID">玩家ID</param>
+    public static void Init(string playerID)
     {
-        
+        //新建存档
+        SaveFile newSave = new SaveFile();
+        newSave.playerID = playerID;
+        newSave.chartScores = new List<ChartScore>();
+        newSave.opreators = new List<Opreator>();
+        //保存
+        SaveToFile(newSave, FILENAME);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 读取存档
+    /// </summary>
+    public static void Open()
     {
-        
+        PlayerSave = LoadFromFile<SaveFile>(FILENAME);
     }
+
+    /// <summary>
+    /// 保存存档
+    /// </summary>
+    public static void Close()
+    {
+        SaveToFile(PlayerSave, FILENAME);
+        PlayerSave = null;
+    }
+
+    /// <summary>
+    /// 判断存档是否存在
+    /// </summary>
+    /// <returns>存档是否存在</returns>
+    public static bool PlayerSaveExists()
+    {
+        return File.Exists(Path.Combine(Application.persistentDataPath, $"{FILENAME}{FILENAME_EXTENSION}"));
+    }
+
+    /// <summary>
+    /// 保存存档
+    /// </summary>
+    /// <param name="data">存档数据</param>
+    /// <param name="saveFileName">存档名</param>
+    private static void SaveToFile(object data, string saveFileName)
+    {
+        string path = Path.Combine(Application.persistentDataPath, $"{saveFileName}{FILENAME_EXTENSION}");
+        string json = JsonUtility.ToJson(data);
+
+        try
+        {
+            File.WriteAllText(path, GZipCompressString(json));
+#if UNITY_EDITOR
+            Debug.Log($"Save data succeed! \n{Application.persistentDataPath}");
+#endif
+        }
+        catch (Exception e)
+        {
+#if UNITY_EDITOR
+            Debug.LogError($"Save data failed!\nPath:{Application.persistentDataPath}\n{e}");
+#endif
+        }
+    }
+
+    /// <summary>
+    /// 存档加载
+    /// </summary>
+    /// <typeparam name="T">存档类</typeparam>
+    /// <param name="saveFileName">存档名</param>
+    /// <returns>存档数据</returns>
+    private static T LoadFromFile<T>(string saveFileName)
+    {
+        string path = Path.Combine(Application.persistentDataPath, $"{saveFileName}{FILENAME_EXTENSION}");
+
+        try
+        {
+            string zippedJson = File.ReadAllText(path);
+            if (zippedJson != null && zippedJson.Substring(0, 2).Equals(FILE_MAGIC_NUMBER))
+            {
+                string json = GZipDecompressString(zippedJson);
+                var data = JsonUtility.FromJson<T>(json);
+#if UNITY_EDITOR
+                Debug.Log($"Load data succeed! \n{data}");
+#endif
+                return data;
+            }
+            return default;
+        }
+        catch (Exception e)
+        {
+#if UNITY_EDITOR
+            Debug.LogError($"Load data failed!\nPath:{Application.persistentDataPath}\n{e}");
+#endif
+            return default;
+        }
+    }
+
+    #region GZip解压缩
+
+    /// <summary>
+    /// GZip压缩+Base64+添加特征签名
+    /// </summary>
+    /// <param name="rawString">原始字符串</param>
+    /// <returns>可存储的字符串</returns>
+    private static string GZipCompressString(string rawString)
+    {
+        if (string.IsNullOrEmpty(rawString) || rawString.Length == 0)
+        {
+            return "";
+        }
+        else
+        {
+            byte[] rawData = Encoding.UTF8.GetBytes(rawString);
+            return $"{FILE_MAGIC_NUMBER}{Convert.ToBase64String(Compress(rawData))}";
+        }
+    }
+
+    /// <summary>
+    /// 去除特征签名+Base64+GZip解压
+    /// </summary>
+    /// <param name="zippedString">存储的字符串</param>
+    /// <returns>原始字符串</returns>
+    private static string GZipDecompressString(string zippedString)
+    {
+        if (string.IsNullOrEmpty(zippedString) || zippedString.Length == 0)
+        {
+            return "";
+        }
+        else
+        {
+            byte[] zippedData = Convert.FromBase64String(zippedString.Remove(0, 2));
+            return Encoding.UTF8.GetString(Decompress(zippedData));
+        }
+    }
+
+    /// <summary>
+    /// GZip压缩
+    /// </summary>
+    /// <param name="rawData">需要压缩的字符串</param>
+    /// <returns>压缩后的字符串</returns>
+    private static byte[] Compress(byte[] rawData)
+    {
+        MemoryStream ms = new MemoryStream();
+        GZipStream compressedzipStream = new GZipStream(ms, CompressionMode.Compress, true);
+        compressedzipStream.Write(rawData, 0, rawData.Length);
+        compressedzipStream.Close();
+        return ms.ToArray();
+    }
+
+    /// <summary>
+    /// Gzip解压
+    /// </summary>
+    /// <param name="zippedData">压缩后的字符串</param>
+    /// <returns>解压后的字符串</returns>
+    private static byte[] Decompress(byte[] zippedData)
+    {
+        MemoryStream ms = new MemoryStream(zippedData);
+        GZipStream compressedzipStream = new GZipStream(ms, CompressionMode.Decompress);
+        MemoryStream outBuffer = new MemoryStream();
+        byte[] block = new byte[1024];
+        while (true)
+        {
+            int bytesRead = compressedzipStream.Read(block, 0, block.Length);
+            if (bytesRead <= 0)
+                break;
+            else
+                outBuffer.Write(block, 0, bytesRead);
+        }
+        compressedzipStream.Close();
+        return outBuffer.ToArray();
+    }
+
+    #endregion GZip解压缩
 }
---
+++ Assets/Scripts/Test/SaveTest.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveTest : MonoBehaviour
+{
+    /// <summary>
+    /// 存档管理器使用说明
+    /// </summary>
+    private void Start()
+    {
+        //存档管理器第一次使用前一定要先初始化（建立存档）。
+        //存档初始化需要提供玩家ID，因此可以在玩家第一次进入游戏，输入玩家ID时进行初始化。
+        //警告：只能使用一次初始化，否则将覆盖已有存档，使用前一定要先通过 SaveManager.PlayerSaveExists() 判断
+        SaveManager.Init(playerID: "CB05");
+
+        //至于如何判断是否第一次进入游戏，可以使用 PlayerSaveExists() 来判断存档是否存在
+        //不存在：第一次进入游戏，存在：那就读取存档吧
+        SaveManager.PlayerSaveExists();
+
+        //存档读取方式：
+        //使用 SaveManager.PlayerSave 来访问存档，但是！！！
+        //首先得调用 SaveManager.Open() 来读取存档，然后才能通过 SaveManager.PlayerSave 来访问存档
+        SaveManager.Open();
+
+        //可以直接使用 SaveManager.PlayerSave.playerID 来访问存档数据
+        Debug.Log($"【直接调用】playID: {SaveManager.PlayerSave.playerID}");
+
+        //当然也能直接通过 SaveManager.PlayerSave.playerID 来修改数据
+        //注意：这里只是演示方便，不要尝试去修改玩家ID！！！
+        SaveManager.PlayerSave.playerID = "Mountain";
+        Debug.Log($"【直接修改数据】playID: {SaveManager.PlayerSave.playerID}");
+
+        //也可以引用一份存档出来操作（强调：是引用）
+        SaveFile file = SaveManager.PlayerSave;
+        Debug.Log($"【引用操作】file.playID: {file.playerID}");
+
+        //关于引用：
+        //引用出来的存档 file 与 SaveManager 提供的 PlayerSave 都指向同一个实例，
+        //使用引用还是直接使用 PlayerSave 都可以
+        SaveManager.PlayerSave.playerID = "Mountain#CB05";
+        Debug.Log($"【设置 SaveManager.PlayerSave，获取 file 】file.playID: {file.playerID}");
+        file.playerID = "Simon#CB05";
+        Debug.Log($"【设置 file，获取 SaveManager.PlayerSave 】playID: {SaveManager.PlayerSave.playerID}");
+
+        //对存档操作完一定要保存到文件，使用 SaveManager.Close() 来保存存档
+        //注意：一定要记得 Close！！！不然存档只会存于内存中，程序结束就没辣！！！
+        SaveManager.Close();
+
+        //总结
+        //先 Open，再读写，最后 Close
+
+        SaveManager.Open();
+
+        Debug.Log($"playID: {SaveManager.PlayerSave.playerID}");
+
+        //读取或者设置存档里的数据等等
+        // ...
+
+        SaveManager.Close();
+
+        //注意：存档读写涉及IO操作，应尽量减少 Open 跟 Close 的次数。
+        //因此，尽量 Open 后统一读取或者设置存档数据，然后再 Close.
+
+        #region 避免这样做
+
+        SaveManager.Open();
+
+        Debug.Log($"playID: {SaveManager.PlayerSave.playerID}");
+
+        SaveManager.Close();
+
+        SaveManager.Open();
+
+        Debug.Log($"playID: {SaveManager.PlayerSave.playerID}");
+
+        SaveManager.Close();
+
+        #endregion 避免这样做
+
+        #region 尽量这样做
+
+        SaveManager.Open();
+
+        Debug.Log($"playID: {SaveManager.PlayerSave.playerID}");
+        Debug.Log($"playID: {SaveManager.PlayerSave.playerID}");
+
+        SaveManager.Close();
+
+        #endregion 尽量这样做
+    }
+}
